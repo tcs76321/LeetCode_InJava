fb7b2f1d60edb1033b4e318ff98ba5a2bd548ed6
Trevor Stahl <trevorclarkstahl@gmail.com>
2020-12-21 22:35:14 -0700

    add the one pass solution to see how it compares

--- number1.java
+++ number1.java
@@ -1,10 +1,21 @@
-//One-Pass Hash Table
+// Two Sum
+
+// One-Pass Hash Table ***from the Solutions***
+// 0 ms, faster than 100.00% of Java online submissions
+// 39.2 MB, less than 53.41% of Java online submissions
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        result[0] = result [1] = -1;
-        
-        
+        Map<Integer, Integer> map = new HashMap<>();
+        int i;
+        for (i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
 
@@ -12,6 +23,9 @@ class Solution {
 //  Runtime: 0 ms
 //      *Wait what? 0ms rt???*
 //  Memory Usage: 39.1 MB
+
+// runtime beats 100.00 % of java submission
+// memory usage beats 64.64 % of java submissions
 
 // class Solution {
 //     public int[] twoSum(int[] nums, int target) {
